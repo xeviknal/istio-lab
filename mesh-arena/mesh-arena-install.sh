bfdb2708ef06714d7def56acc29bb8fd4ee4b024
Xavier Canal i Masjuan <xeviknal@gmail.com>
2019-02-19 19:13:09 +0100

    Mesh-arena fixup

--- mesh-arena/mesh-arena-install.sh
+++ mesh-arena/mesh-arena-install.sh
@@ -4,4 +4,4 @@ oc login -u admin -p admin
 oc new-project mesh-arena
 
 oc adm policy add-scc-to-user privileged -z default
-oc apply -f <(./bin/istioctl kube-inject -f ../mesh-arena.yaml)
+oc apply -f <(./bin/istioctl kube-inject -f ../mesh-arena/mesh-arena.yaml)
