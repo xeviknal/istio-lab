eb7f961605bd54cfb2ef80d01c30ada3406e8e03
Xavier Canal i Masjuan <xeviknal@gmail.com>
2019-06-26 16:51:52 +0200

    Removing kiali operator when uninstalling

--- istio/istio-uninstall.sh
+++ istio/istio-uninstall.sh
@@ -11,3 +11,9 @@ oc delete -f install/kubernetes/istio-demo-auth.yaml
 
 echo 'Deleting crds...'
 for i in install/kubernetes/helm/istio-init/files/crd*yaml; do oc delete -f $i; done
+
+echo 'Uninstall operator and kiali'
+bash <(curl -L https://git.io/getLatestKialiOperator) --uninstall-mode true
+
+echo 'Kicking the tires'
+oc patch kiali kiali -n kiali-operator -p '{"metadata":{"finalizers": []}}' --type=merge
