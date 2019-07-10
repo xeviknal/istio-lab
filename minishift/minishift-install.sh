524dc787295bad045095c02e6165b278ac42d689
Xavier Canal i Masjuan <xeviknal@gmail.com>
2019-07-10 18:13:34 +0200

    Caching more images, decrease network usage in each reinstall

--- minishift/minishift-install.sh
+++ minishift/minishift-install.sh
@@ -10,7 +10,7 @@ minishift addon enable admin-user
 minishift addon enable admissions-webhook
 minishift addon disable anyuid
 
-minishift start --show-libmachine-logs -v5
+minishift start --show-libmachine-logs -v5 --iso-url file:///home/xcanalma/code/minishift-centos7.iso
 minishift ssh -- sudo setenforce 0
 minishift ssh -- sudo sysctl -w vm.max_map_count=262144
 
