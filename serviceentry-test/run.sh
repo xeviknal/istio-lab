b21e5cfeb782d531c321a3f406553dee3fc37fd4
Xavier Canal i Masjuan <xeviknal@gmail.com>
2019-10-22 11:12:04 +0200

    Improve serviceentries examples

--- serviceentry-test/run.sh
+++ serviceentry-test/run.sh
@@ -1,3 +1,8 @@
 APP_NS="stalker"
 
-istioctl kube-inject -f stalkers.yaml | oc apply -n $APP_NS -f -
+oc new-project stalker
+oc adm policy add-scc-to-user privileged -z default -n stalker
+oc adm policy add-scc-to-group privileged system:serviceaccounts -n stalker
+oc adm policy add-scc-to-group anyuid system:serviceaccounts -n stalker
+
+../istio-1.3.3/bin/istioctl kube-inject -f stalkers.yaml | oc apply -n $APP_NS -f -
