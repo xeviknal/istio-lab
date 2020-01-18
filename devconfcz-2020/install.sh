ee95fe930cc3b3398144e381ce4871f2dde55a8b
Xavier Canal i Masjuan <xeviknal@gmail.com>
2020-01-18 11:57:55 +0100

    Travel agency working

---
+++ devconfcz-2020/install.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+PATH=~/code/istio-lab/istio-1.4.3/bin:$PATH
+
+# Installing Istio 1.4.3
+istioctl manifest apply --set profile=demo
+
+# kubernetes edit deployment: version 11 works.
+# add namespace, service, enabled, url (istioctl dashboard jaeger)
+# Adding Jaeger integration
+sleep 10
+echo 'tracing: in_cluster_url: "http://jaeger-query.istio-system.svc:16686"'
+sleep 10
+oc edit kiali kiali -n kiali-operator
